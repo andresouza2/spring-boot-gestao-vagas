f3b12077da84e6eb71b65191d058e0caac509213
andre souza <andssouzza@gmail.com>
2024-01-27 15:43:18 -0400

    feat: add tratamento de erros

--- src/main/java/br/com/andresouza/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/andresouza/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -5,14 +5,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+
 @RestController
-@RequestMapping("/candidates")
+@RequestMapping("/candidate")
 public class CandidateController {
 
   @PostMapping("/")
-  public void create(@RequestBody CandidateEntity candidate) {
+  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
     System.out.println("candidate:");
-    System.out.println(candidate.getEmail());
+    System.out.println(candidateEntity.getEmail());
   }
 
 }
--- src/main/java/br/com/andresouza/gestao_vagas/modules/candidate/controllers/CandidateEntity.java
+++ src/main/java/br/com/andresouza/gestao_vagas/modules/candidate/controllers/CandidateEntity.java
@@ -2,6 +2,11 @@ package br.com.andresouza.gestao_vagas.modules.candidate.controllers;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +14,15 @@ public class CandidateEntity {
 
   private UUID id;
   private String name;
+
+  @NotBlank()
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve ser vazio!")
   private String username;
+
+  @Email(message = "O campo [email] deve conter um email válido!")
   private String email;
+
+  @Length(min = 6, message = "O campo [password] deve conter no mínimo 6 caracteres!")
   private String password;
   private String description;
   private String curriculum;
