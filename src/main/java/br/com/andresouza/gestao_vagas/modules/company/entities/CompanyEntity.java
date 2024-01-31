012c5e7096e265115d293898409a4609521ea8f3
andre souza <andssouzza@gmail.com>
2024-01-30 22:31:06 -0400

    feat: add rotas para criação de Jobs e Companies

--- src/main/java/br/com/andresouza/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/andresouza/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -1,4 +1,4 @@
-package br.com.andresouza.gestao_vagas.modules.candidate.controllers;
+package br.com.andresouza.gestao_vagas.modules.company.entities;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,14 +15,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+@Entity(name = "company")
 @Data
-@Entity(name="candidate")
-public class CandidateEntity {
-
+public class CompanyEntity {
+  
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
-
   private String name;
 
   @NotBlank()
@@ -34,8 +33,9 @@ public class CandidateEntity {
 
   @Length(min = 6, max = 100, message = "O campo [password] deve conter no mínimo 6 caracteres!")
   private String password;
+  
+  private String website;
   private String description;
-  private String curriculum;
 
   @CreationTimestamp
   private LocalDateTime createdAt;
